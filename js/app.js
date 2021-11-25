b4ed914203453b8647e3d723d6ca064230d810d2
astonaqulovsukhrob <astonaqulovsukhro@gmail.com>
2021-11-25 21:07:52 +0300

    battery website

---
+++ js/app.js
@@ -0,0 +1,31 @@
+navigator.getBattery().then((battery) => {
+  const batteryColor = document.querySelector(".battery-color");
+  const batteryFoyiz = document.querySelector(".battery-foyiz");
+  const isCharging = document.querySelector(".isCharging");
+  const batteryBolt = document.querySelector(".battery-bolt");
+
+  const updateAll = () => {
+    updateChargeInfo();
+    updateLevelInfo();
+  };
+
+  updateAll();
+
+  function updateLevelInfo() {
+    batteryColor.style.width = battery.level * 100 + "%";
+    batteryFoyiz.innerHTML = battery.level * 100 + "%";
+  }
+
+  function updateChargeInfo() {
+    isCharging.innerHTML = battery.charging ? "Your derice charging" : "";
+    batteryColor.classList.toggle("active");
+    batteryBolt.classList.toggle("hidden");
+  }
+
+  battery.addEventListener("levelchange", () => {
+    updateLevelInfo();
+  });
+  battery.addEventListener("chargingchange", () => {
+    updateChargeInfo();
+  });
+});
